b0d0dcd596ba1b93ee2781612f02484183770e82
TDROP <tt.thubakgale@gmail.com>
2022-04-07 17:04:08 -0700

    first alx c-program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
- * main -Entry point
- * puts(text) -output to stdout
- * return: Always 0(success)
+ * main - Entry point
+ *
+ * Return: Always 0(success)
  */
-int main (void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
