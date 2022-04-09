2a199f7ab523f1dfb0919ea659f8c795d523cbb4
TDROP <tt.thubakgale@gmail.com>
2022-04-08 19:58:02 -0700

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Return:Always 0(success)
+ */
+int main(void)
+{
+	int num = 48;
+
+	while (num <= 57)
+	{
+		putchar(num);
+		num++;
+	}
+	putchar(10);
+	return (0);
+}
