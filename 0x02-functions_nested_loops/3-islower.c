c1da5101032fc61ff93cadef6c9a521c0917660d
TDROP <tt.thubakgale@gmail.com>
2022-04-12 14:25:28 -0700

    is the parameter lowercase?

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -15,9 +15,9 @@ int _islower(int c)/*c is a parameter of the function*/
 	{
 		if (c == range)
 			return (1);
-		range++;
 		/*if c  exceed 'z' -122 and or is  less than 'a' -97 on the ASCII output 0*/
 		else if (c > 122 || c < 97)
 			return (0);
+		range++;
 	}
 }
