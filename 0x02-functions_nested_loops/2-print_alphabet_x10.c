e1d3370122ae5a8eecf029c9550e4fa63a60a235
TDROP <tt.thubakgale@gmail.com>
2022-04-12 13:47:45 -0700

    alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ *print_alphabet_x10 -Entry point
+ *Description : print lowercase alphabets 10 times with newlines
+ *Return :void (no value)
+ */
+void print_alphabet_x10(void)
+{
+	int lines = 0, letter = 97;
+	/*while for the number of lines*/
+	while (lines < 10)
+	{
+		/*while loop for the alphabets in a sequence*/
+		while (letter < 123)
+		{
+			_putchar(letter)
+			/*increment letter*/
+			letter++;
+		}
+		/*place newline after the alphabet*/
+		_putchar('\n');
+		/*increment lines*/
+		lines++;
+		/*reset the letters value back to 97 */
+		letters = 97;
+	}
+
+}
