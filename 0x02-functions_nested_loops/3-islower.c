cc853342f9621e39f3f1f3be02cbdb2a56382470
TDROP <tt.thubakgale@gmail.com>
2022-04-12 14:23:01 -0700

    is the parameter lowercase?

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *_islower - Entry point
+ *Description: int c is the _islower functions parameter
+ *@c : parameter for the function _islower
+ *Return: 1(is lowercase) or 0 (is not lowercase)
+ */
+int _islower(int c)/*c is a parameter of the function*/
+{
+	int range = 98;
+
+	/*check if its lowercase or not*/
+	while (range < 123)
+	{
+		if (c == range)
+			return (1);
+		range++;
+		/*if c  exceed 'z' -122 and or is  less than 'a' -97 on the ASCII output 0*/
+		else if (c > 122 || c < 97)
+			return (0);
+	}
+}
