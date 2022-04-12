13340e223595150fcddb26e22500871a30a8c15c
TDROP <tt.thubakgale@gmail.com>
2022-04-12 15:08:19 -0700

    sign and return value

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ *print_sign -Entry point
+ *@n: parameter for the print_sign function
+ *Description: program prints the sign of an int if 0 it returns 0
+ *Return: 1 (positive),0 (zero) ,-1 (negative)
+ */
+int print_sign(int n)
+{
+	/*for positives*/
+	if (n > 0)
+	{
+		_putchar('+');
+		_putchar(',');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		_putchar(',');
+		return (0);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		_putchar(',');
+		return (-1);
+	}
+}
