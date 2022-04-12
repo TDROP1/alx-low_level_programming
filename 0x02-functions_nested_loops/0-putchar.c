4df8a74b40f73a2f028ab1fe9b36926c6dfb5e16
TDROP <tt.thubakgale@gmail.com>
2022-04-12 11:33:44 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main -ENtry point
+ * Description: print _putchar exactly
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	printf("%s", "_putchar");
+	return (0);
+}
