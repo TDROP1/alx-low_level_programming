85cf374f5dee90c346565b23e12a00065a7189f4
TDROP <tt.thubakgale@gmail.com>
2022-04-13 00:27:07 -0700

    abs value

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -4,7 +4,7 @@
  *_abs -Entry point
  *Description: program calculates the absolute value
  *@n: parameter
- *Return: Absolute value of parameter 
+ *Return: Absolute value of parameter
  */
 int _abs(int n)
 {
