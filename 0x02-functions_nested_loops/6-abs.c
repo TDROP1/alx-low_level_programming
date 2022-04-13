af92e84fbdf6fce951104751b9c5eae731382eb7
TDROP <tt.thubakgale@gmail.com>
2022-04-13 00:20:19 -0700

    Absolute value

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,18 +1,20 @@
-#!/bin/bash
+#include "main.h"
+
 /**
  *_abs -Entry point
  *Description: program calculates the absolute value
  *@n: parameter
- *Return: 1(positiv);-1(negative);0(zero)
+ *Return: 1(positive),-1(negative),0(zero)
  */
 int _abs(int n)
 {
 	int result;
+
 	if (n > 0)
 		result = n;
 	else if (n == 0)
 		result = 0;
 	else
-		result = n * -1;
+		result = (n * -1);
 	return (result);
 }
