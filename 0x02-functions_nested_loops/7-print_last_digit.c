8655760a5baa31d7fe94a3a4012ffa03bf72b7b1
TDROP <tt.thubakgale@gmail.com>
2022-04-13 00:53:01 -0700

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,11 +8,9 @@
  */
 int print_last_digit(int n)
 {
-	int result;
-	if (n % 1 == n)
-		result = n % 10;
-	else
-		break;
-
+	int result = n % 10;
+	if (result < 0)
+		result = result * -1;
+	_putchar(result + '0');
 	return (result);
 }
