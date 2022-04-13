d4316eb46983f0f7518ca6fd32a9160ff0eb3952
TDROP <tt.thubakgale@gmail.com>
2022-04-13 00:30:59 -0700

    last digit display

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ *print_last_digit - Entry point
+ *@n:parameter
+ *Description:progran returns last digit of a number
+ *Return: value
+ */
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
