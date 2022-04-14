70307ac0abbeb10835ee6b0cf3d8a7350546d585
TDROP <tt.thubakgale@gmail.com>
2022-04-14 02:07:40 -0700

    times of the day

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -20,6 +20,12 @@ void jack_bauer(void)
 				{
 					while (m <= 9)
 					{
+						_putchar('0' + H);
+						_putchar('0' + h);
+						_putchar(':');
+						_putchar('0' + M);
+						_putchar('0' + m);
+						_putchar('\n');
 						m++;
 					}
 					m = 0;
