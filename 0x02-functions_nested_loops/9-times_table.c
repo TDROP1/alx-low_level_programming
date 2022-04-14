9e567ae12a1bbd03a00b0a25bf9defb01f9b2f9f
TDROP <tt.thubakgale@gmail.com>
2022-04-14 10:14:26 -0700

    times table of 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  *times_table -entry point
  *Description:print product of 0 to 9 stops at the 10th multiplication( n * 9)
@@ -7,7 +6,6 @@
  */
 void times_table(void)
 {
-	/*beginning from 0 to 9*/
 	int num = 0;/*must increase by 1 and stop at 9*/
 	int multiplier;
 	int prod;
@@ -30,25 +28,20 @@ void times_table(void)
 			}
 			else if (prod < 10)
 			{
-				_putchar('0' + prod % 0);
+				_putchar('0' + prod % 10);
 				if (multiplier < 9)
-				{
 					_putchar(',');
-				}
-				if (num *(multiplier + 1) >= 10)	
+				if (num * (multiplier + 1) >= 10)
 					_putchar(' ');
-				else 
+				else
 				{
 					_putchar(' ');
 					_putchar(' ');
-
 				}
 			}
-			/*increment multiplier*/
 			multiplier++;
 		}
 		_putchar('\n');
-		/*increment the num by 1*/
 		num++;
 }
 }
