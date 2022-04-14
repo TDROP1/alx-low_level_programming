fc24a5421d27612ad49b35aa0b744e20512e23c4
TDROP <tt.thubakgale@gmail.com>
2022-04-14 13:01:40 -0700

    debugging positive/negative

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main -Entry point
+ * Description:test the case 0 if it will return positive or negative
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	positive_or_negative(i);
+	return (0);
+}
