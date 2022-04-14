c4dba4afbe02e769c62ad3706862fd0c2edb7926
TDROP <tt.thubakgale@gmail.com>
2022-04-14 02:30:08 -0700

    times of the day

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,41 +1,41 @@
 #include "main.h"
 
 /**
- * jack_bauer -Entry point
- * Description: time in hours and minutes
- * Return: Always 0
+ *jack_bauer -entry point
+ *Description: prints times from 00:00 to 23:59
+ *Return: void
  */
 
-void jack_bauer(void)
+int jack_bauer(void)
 {
-	int H = 0, h = 0, M = 0, m = 0;
-
+	/*clock format is Hh:Mm*/
+	int H = 0, h = 0, M =0 , m = 0;
+	int hand = 9;
+	
 	while (H <= 2)
 	{
-		while (h <= 9)
+		if (H == 2)
 		{
-			if (H <= 2 && h <= 3)
+			hand = 3;
+		}
+		while (h <= hand)
+		{
+			while (M <= 5)
 			{
-				while (M <= 5)
+				while (m <= 9)
 				{
-					while (m <= 9)
-					{
-						_putchar('0' + H);
-						_putchar('0' + h);
-						_putchar(':');
-						_putchar('0' + M);
-						_putchar('0' + m);
-						_putchar('\n');
-						m++;
-					}
-					m = 0;
-					M++;
+					_putchar('0' + H);
+					_putchar('0' + h);
+					_putchar(':');
+					_putchar('0' + M);
+					_putchar('0' + m);
+					_putchar('\n');
+					m++;
 				}
+				M++;
 			}
-			M = 0;
 			h++;
 		}
-		h = 0;
 		H++;
 	}
 }
