7e042440f00d29735ccea7f80f62134192a810a5
TDROP <tt.thubakgale@gmail.com>
2022-04-14 13:10:27 -0700

    debugging positive/negative

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -7,10 +7,8 @@
 
 int main(void)
 {
-	printf("entering main\n");
 	int i = 0;
 
-	printf("enter test case is  %d in positive or negative function\n", i);
 	positive_or_negative(i);
 	printf("print after the test case 0\n");
 	return (0);
