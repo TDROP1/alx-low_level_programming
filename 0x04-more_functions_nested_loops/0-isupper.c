7fbafdaf7dae9dd81ab0a0b6058e33af8c8c63a0
TDROP <tt.thubakgale@gmail.com>
2022-04-14 20:27:34 -0700

    FUnction

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isupper - check uppercase
+ * @c: parameter
+ * Return: 1(uppercase),0(not)
+ */
+int _isupper(int c)
+{
+	return (c >= 'A' && c = 'Z');
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,10 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_number(void);
+int _putchar(char c);
+void print_most_numbers(void);
+#endif
